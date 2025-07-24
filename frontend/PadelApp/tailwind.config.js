2f865467920a4f9f643515deefba496f396a7738
Mike <miguelshakur97@gmail.com>
2025-07-24 17:27:56 -0300

    feat: Adding tailwind config

--- frontend/PadelApp/tailwind.config.js
+++ frontend/PadelApp/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#011738",
+        secondary: "#D6FF1D",
+      },
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
