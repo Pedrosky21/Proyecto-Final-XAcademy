449f1aef63d6c7b5e9f27237944d2bab760182d0
PocheG <ignaciogarciapochettino@gmail.com>
2025-07-26 12:43:16 -0300

    fix: router general

--- backend/src/routers/router.ts
+++ backend/src/routers/router.ts
@@ -5,12 +5,13 @@ import positionRouter from '../modules/positions/adapters/PositionsRouter';
 import playerRouter from '../modules/players/adapters/PlayerRouter';
 import userRouter from '../modules/users/adapters/UserRouter';
 import floorMaterialRouter from '../modules/floorMaterials/adapters/MaterialFloorRouter';
+import WallMaterialRouter from '../modules/wallMaterials/adapters/WallMaterialRouter';
 
 const router = Router();
 
 router.use('/categories',categoryRouter);
 router.use('/floor-materials',floorMaterialRouter);
-router.use('/wall-materials',floorMaterialRouter);
+router.use('/wall-materials',WallMaterialRouter);
 router.use('/positions', positionRouter);
 router.use('/players', playerRouter);
 router.use('/users', userRouter);
