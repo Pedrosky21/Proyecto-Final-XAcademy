ecc272a4f86af2ef4e8317d7eb7db016f878369f
PocheG <ignaciogarciapochettino@gmail.com>
2025-08-10 15:33:57 -0300

    feat: marcar turno como reservado

--- frontend/PadelApp/src/app/modules/clubs/view-courts/court-modal/court-modal.component.ts
+++ frontend/PadelApp/src/app/modules/clubs/view-courts/court-modal/court-modal.component.ts
@@ -10,6 +10,9 @@ import { Court } from '../../../../model/Court';
 import { ClubService } from '../../../../core/services/ClubService';
 import { Turn } from '../../../../model/Turn';
 import { TableTurn } from '../../../../components/turn-table/types/TableTurn';
+import { ConfirmationModalService } from '../../../../core/layouts/confirmation-modal/service/confirmationModalService';
+import { loadingScreenService } from '../../../../core/layouts/loading-screen/service/loadingService';
+import { ModalIconEnum } from '../../../../core/layouts/confirmation-modal/models/ModalProps';
 
 @Component({
   selector: 'app-court-modal',
@@ -21,7 +24,10 @@ export class CourtModalComponent implements OnChanges {
   @Input() court: Court | null | undefined = null;
   @Input() columns: string[] = [];
 
-  constructor(private readonly clubService: ClubService) {}
+  constructor(private readonly clubService: ClubService,
+    private readonly confirmationModalService: ConfirmationModalService,
+    private readonly loadingScreenService: loadingScreenService
+  ) {}
   turns: Turn[] | null = null;
   tableTurns: TableTurn[] = [];
 
@@ -103,6 +109,8 @@ export class CourtModalComponent implements OnChanges {
           }),
       };
     });
+    console.log(turns)
+    console.log(this.tableTurns)
   }
 
   changeWeek(next: boolean) {
@@ -150,4 +158,81 @@ export class CourtModalComponent implements OnChanges {
     }
     this.loadTurns();
   }
+  selectedTurn:Turn|undefined=undefined
+  clickRow(event: { weekDay: number; column: string }) {
+    const columnIndex= this.columns.findIndex((col)=>col===event.column)
+    const clickTurn = this.turns!.find((col) => col.date===this.tableTurns[event.weekDay].label && col.startHour===this.columns[columnIndex]);
+
+    this.selectedTurn=clickTurn
+  }
+
+  playerName:string=""
+  markAsReserved() {
+      if (!this.playerName) {
+        this.confirmationModalService.openModal({
+          icon: ModalIconEnum.error,
+          title: 'Error',
+          message: 'Debe ingresar el nombre de la persona que pidio el turno',
+          accept: {
+            title: 'Aceptar',
+            action: () => {
+              this.confirmationModalService.closeModal();
+            },
+          },
+        });
+      } else {
+        this.confirmationModalService.openModal({
+          title: 'Marcar turno como reservado',
+          message: `¿Desea confirmar la reserva del turno a nombre de ${this.playerName}?`,
+          reject: {
+            title: 'Cancelar',
+            action: () => {
+              this.confirmationModalService.closeModal();
+            },
+          },
+          accept: {
+            title: 'Confirmar',
+            action: () => {
+              this.confirmationModalService.closeModal();
+              this.loadingScreenService.showLoadingScreen('Registrando reserva...');
+              this.clubService
+                .markTurnAsReserved(this.selectedTurn?.id!,this.playerName)
+                .subscribe({
+                  next: (response) => {
+                    setTimeout(() => {
+                      this.loadingScreenService.showLoadingScreen(null);
+                      this.confirmationModalService.openModal({
+                        icon: ModalIconEnum.ok,
+                        title: 'Reserva registrada',
+                        message: 'Se ha actualizado el estado del turno con éxito',
+                        accept: {
+                          title: 'Aceptar',
+                          action: () => {
+                            this.confirmationModalService.closeModal();
+                          },
+                        },
+                      });
+                    }, 1500);
+                  },
+                  error: (error) => {
+                    this.loadingScreenService.showLoadingScreen(null);
+                    this.confirmationModalService.openModal({
+                      icon: ModalIconEnum.error,
+                      title: 'Error',
+                      message:
+                        'Ocurrio un problema registrar la reserva. Intente de nuevo mas tarde',
+                      accept: {
+                        title: 'Aceptar',
+                        action: () => {
+                          this.confirmationModalService.closeModal();
+                        },
+                      },
+                    });
+                  },
+                });
+            },
+          },
+        });
+      }
+    }
 }
